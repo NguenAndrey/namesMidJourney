7d9c7dfbe5f134489c7f8202f4424a09bfa5dbe1
NguenAndrey <122989821+NguenAndrey@users.noreply.github.com>
2023-07-03 20:28:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+const openModal1 = document.querySelector('.cards__title1')
+const modal1 = document.querySelector('.modal1')
+const closeModal1 = document.querySelector('.closeModal1')
+
+openModal1.addEventListener('click', () => {
+    modal1.showModal()
+})
+closeModal1.addEventListener('click', () => {
+    modal1.close()
+})
+modal1.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal1) modal1.close()
+})
+
+
+const openModal2 = document.querySelector('.cards__title2')
+const modal2 = document.querySelector('.modal2')
+const closeModal2 = document.querySelector('.closeModal2')
+
+openModal2.addEventListener('click', () => {
+    modal2.showModal()
+    document.body.classList.add('stop_scrolling');
+})
+closeModal2.addEventListener('click', () => {
+    modal2.close()
+    document.body.classList.remove('stop_scrolling');
+})
+modal2.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal2) modal2.close()
+})
+
+const openModal3 = document.querySelector('.cards__title3')
+const modal3 = document.querySelector('.modal3')
+const closeModal3 = document.querySelector('.closeModal3')
+
+openModal3.addEventListener('click', () => {
+    modal3.showModal()
+})
+closeModal3.addEventListener('click', () => {
+    modal3.close()
+})
+modal3.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal3) modal3.close()
+})
+
+const openModal4 = document.querySelector('.cards__title4')
+const modal4 = document.querySelector('.modal4')
+const closeModal4 = document.querySelector('.closeModal4')
+
+openModal4.addEventListener('click', () => {
+    modal4.showModal()
+})
+closeModal4.addEventListener('click', () => {
+    modal4.close()
+})
+modal4.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal4) modal4.close()
+})
+
+const openModal5 = document.querySelector('.cards__title5')
+const modal5 = document.querySelector('.modal5')
+const closeModal5 = document.querySelector('.closeModal5')
+
+openModal5.addEventListener('click', () => {
+    modal5.showModal()
+})
+closeModal5.addEventListener('click', () => {
+    modal5.close()
+})
+modal5.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal5) modal5.close()
+})
+
+const openModal6 = document.querySelector('.cards__title6')
+const modal6 = document.querySelector('.modal6')
+const closeModal6 = document.querySelector('.closeModal6')
+
+openModal6.addEventListener('click', () => {
+    modal6.showModal()
+})
+closeModal6.addEventListener('click', () => {
+    modal6.close()
+})
+modal6.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal6) modal6.close()
+})
+
+const openModal7 = document.querySelector('.cards__title7')
+const modal7 = document.querySelector('.modal7')
+const closeModal7 = document.querySelector('.closeModal7')
+
+openModal7.addEventListener('click', () => {
+    modal7.showModal()
+})
+closeModal7.addEventListener('click', () => {
+    modal7.close()
+})
+modal7.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal7) modal7.close()
+})
+
+const openModal8 = document.querySelector('.cards__title8')
+const modal8 = document.querySelector('.modal8')
+const closeModal8 = document.querySelector('.closeModal8')
+
+openModal8.addEventListener('click', () => {
+    modal8.showModal()
+})
+closeModal8.addEventListener('click', () => {
+    modal8.close()
+})
+modal8.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal8) modal8.close()
+})
+
+const openModal9 = document.querySelector('.cards__title9')
+const modal9 = document.querySelector('.modal9')
+const closeModal9 = document.querySelector('.closeModal9')
+
+openModal9.addEventListener('click', () => {
+    modal9.showModal()
+})
+closeModal9.addEventListener('click', () => {
+    modal9.close()
+})
+modal9.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal9) modal9.close()
+})
+
+const openModal10 = document.querySelector('.cards__title10')
+const modal10 = document.querySelector('.modal10')
+const closeModal10 = document.querySelector('.closeModal10')
+
+openModal10.addEventListener('click', () => {
+    modal10.showModal()
+})
+closeModal10.addEventListener('click', () => {
+    modal10.close()
+})
+modal10.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal10) modal10.close()
+})
+
+const openModal11 = document.querySelector('.cards__title11')
+const modal11 = document.querySelector('.modal11')
+const closeModal11 = document.querySelector('.closeModal11')
+
+openModal11.addEventListener('click', () => {
+    modal11.showModal()
+})
+closeModal11.addEventListener('click', () => {
+    modal11.close()
+})
+modal11.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal11) modal11.close()
+})
+
+const openModal12 = document.querySelector('.cards__title12')
+const modal12 = document.querySelector('.modal12')
+const closeModal12 = document.querySelector('.closeModal12')
+
+openModal12.addEventListener('click', () => {
+    modal12.showModal()
+})
+closeModal12.addEventListener('click', () => {
+    modal12.close()
+})
+modal12.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal12) modal12.close()
+})
